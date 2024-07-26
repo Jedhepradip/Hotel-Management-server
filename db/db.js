1263455d9307716af242d76aef8947dfd489803c
Jedhepradip <149765400+Jedhepradip@users.noreply.github.com>
2024-07-26 12:52:43 +0530

    Update db.js

--- db/db.js
+++ db/db.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 import chalk from "chalk";
-const db = await mongoose.connect("mongodb://localhost:27017/HotelManagement")
+const db = await mongoose.connect("mongodb+srv://pradipjedhe69:pradip@cluster0.mck8iyd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0/test")
 .then(()=>{
     console.log(chalk.green.bold("\n\nDatabase Connection Successful "));
 })
